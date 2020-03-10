87ced89f9dcb573c6b9740ebfcb144b17a59eeea
AlexanderLyaksa <sanchos2004@gmail.com>
2020-03-10 21:44:48 +0300

    refactor flask app

---
+++ webapp/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from webapp.model import db
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    @app.route("/")
+    def hello():
+        return app.config['TEXT']
+    return app
