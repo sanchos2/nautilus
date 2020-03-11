0a91df73d883a70722f2aa0df6530b0eb544e98c
AlexanderLyaksa <sanchos2004@gmail.com>
2020-03-12 00:18:28 +0300

    add receipt module

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,18 +1,36 @@
-from flask import Flask
+import yaml
+import os
+
+from flask import Flask, render_template
 
 from webapp.model import db
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+file_yaml = os.path.join(basedir, '..', 'config.yaml')
+
+with open(file_yaml, 'r') as yml_file:
+    yml_str = yml_file.read()
+    data = yaml.safe_load(yml_str)
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('config.py')
+    app.config.from_mapping(data['DEVELOPMENT'])
     db.init_app(app)
     return app
 
 
-app = create_app()
+appl = create_app()
 
 
-@app.route("/")
-def hello():
-    return app.config['TEXT']
+@appl.route("/")
+def index():
+    title = 'Nautilus'
+    return render_template('index.html', page_title=title, name=appl.config['TEXT'])
+
+@appl.route("/requests")
+def requests():
+    pass
+
+#  run server
+#  set FLASK_APP=webapp && set FLASK_ENV=development && set FLASK_DEBUG=1 && flask run
---
+++ webapp/receipt/find_receipt.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+import urllib.parse as urllib
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def qr_parser(qr_text: str) -> dict:
+    """ Функция получения параметров чека из QRtext"""
+    receipt_data = {}
+    try:
+        data = urllib.urlparse(qr_text)
+        receipt_params = urllib.parse_qs(data.path, strict_parsing=True)
+        for key, values in receipt_params.items():
+            receipt_data[key] = values[0]
+        assert len(receipt_data) == 6
+    except (AssertionError, ValueError, AttributeError):
+        print('Информация с QR кода некорректна')
+        return {}
+    return receipt_data
+
+
+def registration_fns(email: str, name: str, phone: str) -> str:
+    """
+    Функция для регистрации пользователя в ФНС
+    Ответы сервера: 204 - Успешно, 409 -User exist, 500 - Некорректный номер, 400 - некорректный email
+    """
+    registration_url = 'https://proverkacheka.nalog.ru:9999/v1/mobile/users/signup'
+    data = {
+              'email': email,
+              'name': name,
+              'phone': phone
+              }
+    try:
+        registration_request = requests.post(registration_url, json=data)
+        status_code = registration_request.status_code
+        text = registration_request.text
+        if status_code != 204:
+            return text
+        else:
+            return 'Registration OK'
+    except (requests.RequestException, ValueError):
+        return 'Network error or registration failed'
+
+
+def recovery_pass(phone: str) -> str:
+    """"
+    Функция востановления пароля ФНС
+    Ответы сервера: 204 - Успешно, 404 - Not found
+    """
+    recovery_url = 'https://proverkacheka.nalog.ru:9999/v1/mobile/users/restore'
+    data = {'phone': phone}
+    try:
+        recovery_request = requests.post(recovery_url , json=data)
+        status_code = recovery_request.status_code
+        text = recovery_request.text
+        if status_code != 204:
+            return text
+        else:
+            return 'Recovery password OK. Wait SMS'
+    except (requests.RequestException, ValueError):
+        return 'Network error or registration failed'
+
+
+def check_receipt(receipt_data: dict) -> str:
+    """
+    Функция проверки валидности чека
+    Ответы сервера: 204 - Чек найден, 406 - Не найден или сумма(дата) некоректны, 400 - Не указанна дата(сумма)
+    """
+
+    date_string = receipt_data['t']
+    date = datetime.strptime(date_string, '%Y%m%dT%H%M%S')
+    format_date = date.strftime('%Y-%m-%dT%H:%M')
+    try:
+        url_check_receipt = 'https://proverkacheka.nalog.ru:9999/v1/ofds/*/inns/*/fss/' + receipt_data['fn'] + \
+                            '/operations/' + receipt_data['n'] + '/tickets/' + receipt_data['i'] + '?fiscalSign=' +\
+                            receipt_data['fp'] + '&date=' + format_date + '&sum=' + receipt_data['s']
+    except KeyError:
+        return 'Key Error'
+    try:
+        check_receipt = requests.get(url_check_receipt)
+        status_code = check_receipt.status_code
+        text = check_receipt.text
+        if status_code != 204:
+            return text or 'Empty response'
+        else:
+            return 'Receipt OK'
+    except (requests.RequestException, ValueError):
+        return 'Network error'
+
+
+def get_receipt(receipt_data: dict, login: str, password: int) -> dict:
+    """ Функция получения полных данных по кассовому чеку"""
+    headers = {'device-id': '',
+               'device-os': '',
+               }
+    try:
+        url_receipt = 'https://proverkacheka.nalog.ru:9999/v1/inns/*/kkts/*/fss/' + receipt_data['fn'] + \
+                      '/tickets/' + receipt_data['i'] + '?fiscalSign=' + receipt_data['fp'] + \
+                      '&sendToEmail=no'
+    except KeyError:
+        return {}
+
+    try:
+        full_receipt = requests.get(url_receipt, auth=HTTPBasicAuth(login, password), headers=headers)
+        full_receipt.raise_for_status()
+        data = full_receipt.json()
+        return data
+    except (requests.RequestException, ValueError):
+        return {}
